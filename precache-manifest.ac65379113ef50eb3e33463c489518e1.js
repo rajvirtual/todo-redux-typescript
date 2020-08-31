4e3f91a59dd11c2dcde6f571eff5eb2c2374a246
Raj Vijay <raj@virtualbeans.com>
2020-08-30 22:10:05 -0400

    Updates

---
+++ precache-manifest.ac65379113ef50eb3e33463c489518e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab1da3499b310c1bea48cb1b9bb5c5b",
+    "url": "/todo-redux-typescript/index.html"
+  },
+  {
+    "revision": "72f552bba16a0ad9082b",
+    "url": "/todo-redux-typescript/static/css/main.6b82b46b.chunk.css"
+  },
+  {
+    "revision": "89a1a5271f81032e1565",
+    "url": "/todo-redux-typescript/static/js/2.471d543e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-redux-typescript/static/js/2.471d543e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72f552bba16a0ad9082b",
+    "url": "/todo-redux-typescript/static/js/main.d840d518.chunk.js"
+  },
+  {
+    "revision": "d449863297fa041123b0",
+    "url": "/todo-redux-typescript/static/js/runtime-main.e0099b6e.js"
+  }
+]);
